7a5788da95fa1bee285a4ee682b47ac90bbede37
diegohenao819 <diegohenao819@gmail.com>
2024-03-09 21:22:57 -0500

    gitignore

--- index.js
+++ index.js
@@ -1,40 +1,33 @@
+const { conexion } = require("./basedatos/conexion");
+require("dotenv").config();
+const cors = require("cors");
 
-const {conexion} = require('./basedatos/conexion'  );
-require('dotenv').config();
-const cors = require('cors');
-
-
-const express = require('express');
-
+const express = require("express");
 
 // conectar a la base de datos
 conexion();
 
-
 const app = express();
 
 // configurar el cors
-app.use( cors() );
+app.use(cors());
 // convertir el body a json
-app.use( express.json() );
-app.use( express.urlencoded({ extended: true }) );
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // rutas
 const rutas_articulo = require("./rutas/articulo_rutas");
 
 // Cargo Las rutas
-app.use( "/api", rutas_articulo );
+app.use("/api", rutas_articulo);
 
-app.get("/probando", (req, res) => (
-    res.status(200).json({
-        mensaje: "Probando el servidor de Node y Express"
-    })
-));
+app.get("/probando", (req, res) =>
+  res.status(200).json({
+    mensaje: "Probando el servidor de Node y Express....",
+  })
+);
 
 // iniciar el servidor
-app.listen( process.env.PORT || 3000, () => {
-
-    console.log(`Servidor corriendo en el puerto ${ process.env.PORT }`);
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Servidor corriendo en el puerto ${process.env.PORT}`);
 });
-
-
