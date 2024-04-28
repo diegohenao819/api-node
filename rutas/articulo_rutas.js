4da993c371491d18aea8b161f1b9e6c3663b9dda
diegohenao819 <diegohenao819@gmail.com>
2024-04-28 10:05:25 -0500

    cambio de ruta en Uploads

--- rutas/articulo_rutas.js
+++ rutas/articulo_rutas.js
@@ -1,19 +1,21 @@
 const express = require("express");
 const multer = require("multer");
 const articuloControlador = require("../controladores/articulo_controlador");
+const ImageModel = require("../modelos/image.model");
 
 const router = express.Router();
 
 // MULTER
-const almacenamiento = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "./imagenes/articulos/");
-  },
-  filename: function (req, file, cb) {
-    cb(null, "articulo" + Date.now() + file.originalname);
+const Storage = multer.diskStorage({
+  destination: "uploads",
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
   },
 });
-const subidas = multer({ storage: almacenamiento });
+
+const upload = multer({
+  storage: Storage
+}).single("testImage");
 
 // Ruta de prueba
 router.get("/rutaPrueba", articuloControlador.prueba);
@@ -23,11 +25,30 @@ router.get("/articulos/:ultimos?", articuloControlador.listar);
 router.get("/articulo/:id", articuloControlador.uno);
 router.delete("/articulo/:id", articuloControlador.borrar);
 router.put("/articulo/:id", articuloControlador.editar);
-router.post(
-  "/subir-imagen/:id",
-  [subidas.single("file0")],
-  articuloControlador.subir
-);
+router.post("/upload", (req, res) => {
+  upload(req, res, (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      const newImage = new ImageModel({
+        name: req.body.name,
+        image: {
+          data: req.file.filename,
+          contentType: "image/png"
+        }
+      })
+      newImage.save()
+        .then(() => {
+          res.send("Imagen guardada");
+        })
+        .catch((err) => {
+          console.log(err);
+        })
+    }
+
+  })
+})
+
 router.get("/imagen/:fichero", articuloControlador.imagen);
 
 module.exports = router;
